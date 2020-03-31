ebf7ddb81815b9b60b9708fa4657a27658ecc505
Dominik Ringler <>
2020-03-31 16:03:24 +0100

    Fix test crash

--- Tests/Test Data/AVAudioPlayer+Mock.swift
+++ Tests/Test Data/AVAudioPlayer+Mock.swift
@@ -8,9 +8,12 @@
 
 import AVFoundation
 
+private final class TestBundle { }
 extension AVAudioPlayer {
     
     static func mock() -> AVAudioPlayer {
-        return try! AVAudioPlayer(contentsOf: URL(string: "Sample")!)
+        let bundle = Bundle(for: TestBundle.self)
+        let path = bundle.path(forResource: "Sample", ofType: "mp3")!
+        return try! AVAudioPlayer(contentsOf: URL(string: path)!)
     }
 }
